f6e6abdf7addffb6914b738e1a869b1f9eb4459e
Vestry <vestry.odessa@gmail.com>
2021-05-03 20:14:38 +0300

    chore: set up code highlight and add code component

---
+++ src/components/Code.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="code__wrapper">
+    <highlightjs language='javascript' :code="codeFragment" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Code",
+    props: {
+      codeFragment: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .code__wrapper {
+    max-width: 800px;
+    width: 80%;
+    border-radius: 20px;
+    box-shadow: 1px 1px 5px 3px rgba(0,0,0,.1);
+    margin: 2em auto;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router';
+import Vuikit from "vuikit";
+import VuikitIcons from "@vuikit/icons";
+import hljs from 'highlight.js'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuikit);
+Vue.use(VuikitIcons);
+Vue.use(hljs.vuePlugin);
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
