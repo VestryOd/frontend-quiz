e4a497893cd4e071d64a6885d37414c6f12ac98d
Vestry <vestry.odessa@gmail.com>
2021-08-27 22:04:56 +0300

    refactor: change header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="header">
-    <div class="logo"></div>
-<!--    <router-link-->
-<!--            v-for="item in navItems"-->
-<!--            :to="item.path"-->
-<!--            class="router-link"-->
-<!--            :key="item.id"-->
-<!--    >-->
-<!--      {{ item.name }}-->
-<!--    </router-link>-->
-    <MainNav :items="navItems" />
-    <div class="profile-icon"></div>
+    <div class="container space-between">
+      <Logo />
+      <MenuButton :is-opened="isMenuOpened" @click.native="toggleMenu" />
+    </div>
   </div>
 </template>
 
 <script>
   import { routerConfig } from "../constants/configs.js";
-  import MainNav from "./MainNav";
+  import Logo from "./Logo";
+  import MenuButton from "./MenuButton";
 
   export default {
     name: "Header",
     props: {},
+    data() {
+      return {
+        isMenuOpened: false,
+      }
+    },
     computed: {
       navItems() {
         return routerConfig;
       },
     },
     components: {
-      MainNav,
+      Logo,
+      MenuButton,
     },
     mounted() {
       console.log('--mounted', this.navItems);
+    },
+    methods: {
+      toggleMenu() {
+        console.log('--menu toggled');
+        this.isMenuOpened = !this.isMenuOpened;
+      }
     }
   }
 </script>
