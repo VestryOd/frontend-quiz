132a123f0b86a5ab0843fe9b811a564ab9a247bd
mkaspriv <mkaspriv@deloitte.com>
2024-08-27 10:49:55 +0200

    feat: add quiz answers counter, fixed theory links layouts, highlighted right answer on wrong

---
+++ src/components/QuizCounter.vue
@@ -0,0 +1,31 @@
+<template>
+  <section id="quiz-answers-counter" class="quiz-counter__wrapper">
+    <div class="quiz-counter">
+      <span class="quiz-counter__right">{{right}}</span>
+      <span>/</span>
+      <span class="quiz-counter__wrong">{{wrong}}</span>
+      <span>/</span>
+      <span>{{total}}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'QuizCounter',
+  props: {
+    total: {
+      type: Number,
+      default: 0
+    },
+    right: {
+      type: Number,
+      default: 0
+    },
+    wrong: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
